b5a87130feb4940b96944a99bb9c1e7004841aac
Ruukaze <joyabrianrantepasang14@gmail.com>
2024-01-23 16:53:30 +0800

    video 4 : transform, transition, animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,16 @@ module.exports = {
       fontFamily: {
         inter: ['Inter']
       },
+      animation: {
+        'spin-slow': 'spin 3s linear infinite',
+        'wiggle': 'wiggle 1s ease-in-out infinite'
+      },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%' : {transform: 'rotate(3deg)' },
+        },
+      },
     },
   },
   plugins: [],
